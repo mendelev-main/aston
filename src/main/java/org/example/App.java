216c8ecc770cc63b2367cc5f7a2716b1d81df159
Aleksandr Mendelev <mendelev.main@gmail.com>
2025-03-18 17:43:23 +0300

    changed the bd now postgres instead of h2

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -5,6 +5,8 @@ import java.util.List;
 public class App {
     public static void main(String[] args) {
 
+        CRUD.createEmployee(new Employee(0, "Иванова", "Дарья"));
+
         List<Employee> employees = CRUD.getEmployee();
         System.out.println(employees);
 
